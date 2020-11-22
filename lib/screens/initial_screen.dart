04d14f0c02ac7f537cd69d6d6609f1453032c4dc
Ariel Tay <shihqian@gmail.com>
2020-11-22 17:59:52 +0800

    Extract initial screen to a standalone file under screens

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_workshop/screens/todo_screen.dart';
+
+class InitialScreen extends StatelessWidget {
+  static String id = 'initial_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Ariel's Workshop")),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              children: <Widget>[
+                Image(
+                  image: AssetImage('images/app_main.png'),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    print('to reroute to new screen');
+                    Navigator.pushNamed(context, ToDoListScreen.id);
+                  },
+                  color: Colors.amber[500],
+                  padding: EdgeInsets.all(10.0),
+                  child: Center(
+                    child: Text(
+                      'Todo-App',
+                      style: TextStyle(color: Colors.black, fontSize: 20.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
