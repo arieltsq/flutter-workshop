c3b0f4825b183f6537178401da4f9973bc743a32
Ariel Tay <shihqian@gmail.com>
2020-11-22 18:00:36 +0800

    Route Todo and Initial Screen in Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workshop/screens/initial_screen.dart';
+import 'package:flutter_workshop/screens/todo_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,47 +11,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        // Define the default font family.
-        fontFamily: 'Cabin',
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          // Define the default font family.
+          fontFamily: 'Cabin',
 
-        // Define the default TextTheme. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: TextTheme(
-          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Cabin'),
+          // Define the default TextTheme. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          textTheme: TextTheme(
+            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Cabin'),
+          ),
         ),
-      ),
-      home: Scaffold(
-          appBar: AppBar(title: Text("Ariel's Workshop")),
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                children: <Widget>[
-                  Image(
-                    image: AssetImage('images/app_main.png'),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      print('to reroute to new screen');
-                    },
-                    color: Colors.amber[500],
-                    padding: EdgeInsets.all(10.0),
-                    child: Center(
-                      child: Text(
-                        'Todo-App',
-                        style: TextStyle(color: Colors.black, fontSize: 20.0),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )),
-    );
+        initialRoute: InitialScreen.id,
+        routes: {
+          ToDoListScreen.id: (context) => ToDoListScreen(),
+          InitialScreen.id: (context) => InitialScreen(),
+        });
   }
 }
