e488423ab901d91e8ba9ca47e6420aa2f1e34d22
Ariel Tay <shihqian@gmail.com>
2020-11-27 00:56:18 +0800

    Refactor Add to do modal

--- lib/components/add_todo_modal.dart
+++ lib/components/add_todo_modal.dart
@@ -4,48 +4,43 @@ class AddTodoModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String newItemTitle;
-    return Container(
+    return Padding(
+        padding: MediaQuery.of(context).viewInsets,
         child: Container(
-      padding: EdgeInsets.all(30.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text('Add Items',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 55.0,
-              )),
-          TextFormField(
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 25.0,
-            ),
-            autofocus: true,
-            textAlign: TextAlign.center,
-            onChanged: (newText) {
-              newItemTitle = newText;
-            },
+          padding: EdgeInsets.all(30.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
           ),
-          SizedBox(height: 20.0),
-          FlatButton(
-            onPressed: () {
-              print('Todo new: $newItemTitle');
-              Navigator.pop(context);
-            },
-            color: Colors.black,
-            child: Text(
-              'Add',
-              style: TextStyle(
-                color: Colors.amber,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Add Items',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline6),
+              TextFormField(
+                style: Theme.of(context).textTheme.bodyText2,
+                autofocus: true,
+                textAlign: TextAlign.center,
+                onChanged: (newText) {
+                  newItemTitle = newText;
+                },
               ),
-            ),
-          )
-        ],
-      ),
-    ));
+              SizedBox(height: 20.0),
+              FlatButton(
+                onPressed: () {
+                  print('Todo new: $newItemTitle');
+                  Navigator.pop(context);
+                },
+                color: Colors.black,
+                child: Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.amber,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
