30e434705d913629620b7bc5c87e366d1e0b2d1b
Ariel Tay <shihqian@gmail.com>
2020-11-27 12:34:05 +0800

    Update static index to dynamic index - value getting from state management

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_workshop/components/add_todo_modal.dart';
-import 'package:flutter_workshop/models/item.dart';
+import 'package:flutter_workshop/models/item_data.dart';
 import 'package:flutter_workshop/widgets/item_list.dart';
+import 'package:provider/provider.dart';
 
 class ToDoListScreen extends StatefulWidget {
   static String id = 'todo_list_screen';
@@ -10,6 +11,7 @@ class ToDoListScreen extends StatefulWidget {
 }
 
 class _ToDoListScreenState extends State<ToDoListScreen> {
+  int itemsIndex = ItemData().itemCount;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +23,7 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text('Items to do: 3',
+                Text('Items to do: ${Provider.of<ItemData>(context).itemCount}',
                     style: Theme.of(context).textTheme.headline6),
                 Text('To do:', style: Theme.of(context).textTheme.subtitle1),
                 SizedBox(height: 200.0, child: ItemList()),
